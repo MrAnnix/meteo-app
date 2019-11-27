adb9138d13c31732019e6c1f0525280b92802afc
Raúl San Martín Aniceto <9783500+MrAnnix@users.noreply.github.com>
2019-11-27 17:32:43 +0100

    Update app.js

--- app.js
+++ app.js
@@ -19,6 +19,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Bootstrap 4 and libraries
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist'));
+app.use('/js', express.static(__dirname + '/node_modules/popper.js/dist'));
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
+
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
