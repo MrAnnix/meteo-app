96db9dd65348431835ab43e3c295ca0bb5849629
Raúl San Martín Aniceto <9783500+MrAnnix@users.noreply.github.com>
2019-11-28 16:39:45 +0100

    Added things

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'meteo' });
+  let location = req.body.location;
+  res.render('index', { title: 'meteo' }, location );
 });
 
 module.exports = router;
