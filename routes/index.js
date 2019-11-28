21fee0dc33167ee3d93f1e6f55380a2bcc5216a5
Raúl San Martín Aniceto <9783500+MrAnnix@users.noreply.github.com>
2019-11-28 17:01:10 +0100

    Get method works

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  let location = req.body.location;
-  res.render('index', { title: 'meteo' }, location );
+  let location = req.query.location
+  res.render('index', { title: 'meteo', location });
 });
 
 module.exports = router;
