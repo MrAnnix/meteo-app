613c11291845e8c83a9031b62e3488c100e62434
Raúl San Martín Aniceto <9783500+MrAnnix@users.noreply.github.com>
2019-11-28 17:20:04 +0100

    Added location

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var geoip = require('geoip-lite');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   let location = req.query.location
-  res.render('index', { title: 'meteo', location });
+  let ip = req.connection.remoteAddress
+  console.log(ip);
+  let geo = geoip.lookup(ip)
+  if (geo){
+    var city = geo.city
+  }
+  res.render('index', { title: 'meteo', location, city });
 });
 
 module.exports = router;
