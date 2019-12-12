fee84a2f658e7de964ad68d1a69d8b32576a90db
Raúl San Martín Aniceto <9783500+MrAnnix@users.noreply.github.com>
2019-12-12 20:59:40 +0100

    Added geonames city resolution

--- routes/detailed.js
+++ routes/detailed.js
@@ -1,5 +1,8 @@
+var request = require('request');
 var express = require('express');
 var router = express.Router();
+
+var APIKeys = require('../data/APIKeys.json');
 
 /* GET detailed block. */
 router.get('/', function(req, res, next) {
@@ -7,7 +10,20 @@ router.get('/', function(req, res, next) {
   let longitude = req.query.latitude;
   let latitude = req.query.longitude;
   
-  res.render('detailed', { title: 'meteo', location, longitude, latitude });
+  let url = 'http://api.geonames.org/geoCodeAddressJSON?q='+location+'&country=ES&maxRows=1&username='+APIKeys.geonames_api_key;
+  console.log(url);
+  request(url, function (err, response, body) {
+    if(err){
+      res.render('detailed', { title: 'meteo', error: 'An error has occurred'});
+    } else {
+      let place = JSON.parse(body)
+      if(place.address.adminName3 == undefined){
+        res.render('detailed', { title: 'meteo', error: 'An error has occurred'});
+      } else {
+        res.render('detailed', { title: 'meteo', location: place.address.adminName3, longitude, latitude });  
+      }
+    }
+  });
 });
 
 module.exports = router;
