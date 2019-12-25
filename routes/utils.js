5a28f59febd17e937c6565e42e6f745baba33bbc
MrAnnix <hello@raulsanmartin.me>
2019-12-25 12:51:36 +0100

    Added comments

--- routes/utils.js
+++ routes/utils.js
@@ -6,6 +6,7 @@ var APIKeys = require('../data/APIKeys.json');
 var municipiosData = require('../data/newMunicipios.json');
 
 
+/* Efficient repacement based on regexp */
 String.prototype.replaceAll = function(str1, str2, ignore) 
 {
   return this.replace(new RegExp(str1.replace(/([\/\,\!\\\^\$\{\}\[\]\(\)\.\*\+\?\|\<\>\-\&])/g,"\\$&"),(ignore?"gi":"g")),(typeof(str2)=="string")?str2.replace(/\$/g,"$$$$"):str2);
